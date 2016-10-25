94bd828132208b5307b8acbdf72a147c0ccaa1fd
Regina Galvez Chavez <reginagalvezchavez@MBP-de-Regina.iptv.microsoft.com>
2016-10-25 15:37:23 -0300

    hasta password

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	
+	var name = document.getElementById("name").value;
+	if( name == null || name.length == 0|| /^\s+$/.test(name) ) {
+		alert('Parece que no has ingresado tu nombre');
+		return false;
+	}
+	var lastname = document.getElementById('lastname').value;
+	if( lastname == null || lastname.length == 0|| /^\s+$/.test(lastname) ) {
+		alert('Parece que no has ingresado tu apellido');
+		return false;
+	}
+
+	var password = document.getElementById('input-password').value;
+	if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(password)) ) {
+		alert('Parece que no has ingresado una contraseña');
+		return false;
+	}
+
+
+
+
 }
+
