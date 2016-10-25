cb26b3ee8bcf85bc871284580e550cacc8ef9393
Regina Galvez Chavez <reginagalvezchavez@MBP-de-Regina.iptv.microsoft.com>
2016-10-25 20:28:55 -0300

    falta validar que se seleccione una opción

--- js/main.js
+++ js/main.js
@@ -11,19 +11,34 @@ function validateForm(){
 		return false;
 	}
 
-	var email = document.getElementById('input-email').value;
-	if( email.length == 0){
-      alert('Parece que no has ingresado un email valido');
+	var pass = document.getElementById('input-password');
+	if( pass.value =="" ){
+		alert('ingresa una contraseña');
+		return false;
+
+	}
+	var mail = document.getElementById('input-email').value;
+	if(mail.length == 0){
+		alert('ingresa tu email');
+		return false;
+	}
+	if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(mail)) ) {
 		return false;
 	}
 
-	if(!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))) {
+	if(pass!= 123456 || pass!= 098754){
+		if(pass.length > 6){
+			return true;
+		}else{
+			alert('No puedes usar 123456 ni 098754');
+			return false;
+		}
+	}else{
+		alert('No puedes usar 123456 ni 098754');
 		return false;
 	}
-
-	
-
+	var qbici = document.getElementsByTagName('select').value;
+	if ( qbici == null || qbici == 0 ){
+     alert("selecciona una opcion");
+	}
 }
-
-	
-
