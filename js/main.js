9943dd347f18180d8631761848dd268ba4631ab4
Regina Galvez Chavez <reginagalvezchavez@MBP-de-Regina.iptv.microsoft.com>
2016-10-25 22:34:01 -0300

    esto-seria

--- js/main.js
+++ js/main.js
@@ -5,27 +5,20 @@ function validateForm(){
 		alert('Parece que no has ingresado tu nombre');
 		return false;
 	}
+	
 	var lastname = document.getElementById('lastname').value;
 	if( lastname == null || lastname.length == 0|| /^\s+$/.test(lastname) ) {
 		alert('Parece que no has ingresado tu apellido');
 		return false;
 	}
 
-	var pass = document.getElementById('input-password');
-	if( pass.value =="" ){
+	var pass = document.getElementById('input-password').value;
+	if( pass == 0 ){
 		alert('ingresa una contraseña');
 		return false;
 
 	}
-	var mail = document.getElementById('input-email').value;
-	if(mail.length == 0){
-		alert('ingresa tu email');
-		return false;
-	}
-	if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(mail)) ) {
-		return false;
-	}
-
+	
 	if(pass!= 123456 || pass!= 098754){
 		if(pass.length > 6){
 			return true;
@@ -37,8 +30,18 @@ function validateForm(){
 		alert('No puedes usar 123456 ni 098754');
 		return false;
 	}
-	var qbici = document.getElementsByTagName('select').value;
-	if ( qbici == null || qbici == 0 ){
-     alert("selecciona una opcion");
+
+	var mail = document.getElementById('input-email').value;
+	if(mail.length == 0){
+		alert('ingresa tu email');
+		return false;
+	}
+	if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(mail)) ) {
+		return false;
+	}
+	var elije = document.getElementsByClassName('form-control').value;
+	if( elije == null || elije == 0){
+		alert('elije una opcion');
+		return false;
 	}
 }
